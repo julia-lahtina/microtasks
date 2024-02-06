41ff3f6670e30a640e39b0144f841dff08303408
julia-lahtina <j.lakhtin@gmail.com>
2024-02-06 18:38:37 +0100

    add new component for task method filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Button} from "./components/Button";
 import {UseState} from "./components/UseState";
 import {Map} from "./components/Map";
+import {CurrentMoneyType, Filter, MoneyType} from "./components/Filter";
 
+export type FilterType = "all" | "dollar" | "ruble"
 
 function App() {
 
@@ -11,37 +13,67 @@ function App() {
             console.log(name)
         }*/
 
-    const Button1Foo = (subscriber: string, age: number, address: string) => {
-        console.log(subscriber, age, address)
-    }
-    const Button2Foo = (subscriber: string) => {
-        console.log(subscriber)
-    }
-    const Button3Foo = () => {
-        console.log("I am stupid Button")
+    /*    const Button1Foo = (subscriber: string, age: number, address: string) => {
+            console.log(subscriber, age, address)
+        }
+        const Button2Foo = (subscriber: string) => {
+            console.log(subscriber)
+        }
+        const Button3Foo = () => {
+            console.log("I am stupid Button")
+        }
+
+        const books = [
+            {id: 1, type: "roman", author: "Van de Kamp", published: 2022},
+            {id: 2, type: "horror", author: "King", published: 1995},
+            {id: 3, type: "roman", author: "Dostoevski", published: 2010},
+        ]*/
+
+    const [money, setMoney] = useState([
+        {banknote: "dollar", nominal: 100, number: "a123456789"},
+        {banknote: "dollar", nominal: 50, number: "b123456789"},
+        {banknote: "ruble", nominal: 100, number: "c123456789"},
+        {banknote: "dollar", nominal: 100, number: "d123456789"},
+        {banknote: "dollar", nominal: 50, number: "e123456789"},
+        {banknote: "ruble", nominal: 100, number: "f123456789"},
+        {banknote: "dollar", nominal: 50, number: "j123456789"},
+        {banknote: "ruble", nominal: 50, number: "h123456789"},
+    ])
+
+    const [filter, setFilter] = useState<FilterType>("all")
+
+
+    let currentMoney = money;
+
+    if (filter === "ruble") {
+        currentMoney = money.filter(filteredMoney => filteredMoney.banknote === "ruble")
     }
 
-    const books = [
-        {id: 1, type: "roman", author: "Van de Kamp", published: 2022},
-        {id: 2, type: "horror", author: "King", published: 1995},
-        {id: 3, type: "roman", author: "Dostoevski", published: 2010},
-    ]
+    if (filter === "dollar") {
+        currentMoney = money.filter(filteredMoney => filteredMoney.banknote === "dollar")
+    }
+    const onClickFilterHandler = (nameButton: FilterType) => {
+        setFilter(nameButton)
+    }
 
     return (
-        <div className={"App"}>
-            <Button name={"MyYouTubeChannel-1"} callBack={() => Button1Foo("I'm Vasya", 21, "live in Minsk")}/>
-            <Button name={"MyYouTubeChannel-2"} callBack={() => Button2Foo("I'm Ivan")}/>
-            <Button name={"Stupid"} callBack={Button3Foo}/>
+        <>
+            <Filter currentMoney={currentMoney} onClickFilterHandler={onClickFilterHandler}/>
 
-            <UseState/>
-            <Map books={books}/>
+        </>
+        /*                            <Button name={"MyYouTubeChannel-1"} callBack={() => Button1Foo("I'm Vasya", 21, "live in Minsk")}/>
+                    <Button name={"MyYouTubeChannel-2"} callBack={() => Button2Foo("I'm Ivan")}/>
+                    <Button name={"Stupid"} callBack={Button3Foo}/>
+
+                    <UseState/>
+                    <Map books={books}/>
 
 
-            {/*            <button onClick={(event) => onClickHandler("Vasya")}>MyYouTubeChannel-2</button>
-            <button onClick={(event) => onClickHandler("Ivan")}>MyYouTubeChannel-3</button>*/}
-        </div>
-    );
+                                    <button onClick={(event) => onClickHandler("Vasya")}>MyYouTubeChannel-2</button>
+                    <button onClick={(event) => onClickHandler("Ivan")}>MyYouTubeChannel-3</button>*/
+
+
+    )
 }
-
 
 export default App;
