b89fa10b8181a4a56385664d5675d93a8d013529
julia-lahtina <j.lakhtin@gmail.com>
2024-02-06 19:26:56 +0100

    change component input

--- src/components/FullInput.tsx
+++ src/components/FullInput.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {ChangeEvent, ChangeEventHandler, useState} from 'react';
 
-export const FullInput = () => {
+export type FullInputPropsType = {
+    addMessage: ()=> void
+}
+export const FullInput = (props: FullInputPropsType) => {
+
+    let [title, setTitle] = useState("")
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
     return (
         <div>
-            <input type="text"/>
-            <button>+</button>
+            <input onChange={onChangeInputHandler} type="text"/>
+            <button onClick={()=>{}}>+</button>
         </div>
     );
 };
