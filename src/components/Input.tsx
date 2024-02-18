ba74db7dfc39c861b711dccac3c629fef488f501
julia-lahtina <j.lakhtin@gmail.com>
2024-02-18 11:06:40 +0100

    add ref

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,22 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, RefObject} from 'react';
 
 export type InputType = {
 /*    value: string
     callBack: (value: string) => void*/
-    newTitle: string
-    setNewTitle: (newTitle: string) => void
+    newTitle: RefObject<HTMLInputElement>
+    //setNewTitle: (newTitle: string) => void
 }
 export const Input = (props: InputType) => {
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+/*    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
        props.setNewTitle(e.currentTarget.value)
-    }
+    }*/
 
     return (
-        <input onChange={onChangeHandler} value={props.newTitle} type="text"/>
+        <input
+            ref={props.newTitle}
+            //onChange={onChangeHandler}
+            //value={props.newTitle} type="text"
+        />
     );
 };
