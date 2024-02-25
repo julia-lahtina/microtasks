5979fe8bee911bd041adffe6a1aad0d8d66f6937
julia-lahtina <j.lakhtin@gmail.com>
2024-02-25 13:32:31 +0100

    delete unused components

--- src/components/Items.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {Item} from './Item';
-import {BooksType} from '../App';
-
-export const Items = (props: BooksType) => {
-    return (
-        <Item books={props.books}/>
-    );
-};
