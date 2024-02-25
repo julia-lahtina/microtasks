2e9fa66efb6947919affce332b1fee2b29de22b2
julia-lahtina <j.lakhtin@gmail.com>
2024-02-25 13:33:54 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,93 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Items} from './components/Items';
-import {Button} from './components/Button';
-import {Input} from './components/Input';
-
-export type BookPropsType = {
-    id: number
-    title: string
-    author: string
-    nameOfBook: string
-    yearOfPublishing: number
-    isChecked: boolean
-}
-
-export type BooksType = {
-    books: BookPropsType[]
-}
-
+import {Modal} from './components/modal/Modal';
+import {SuperButton} from './components/superButton/SuperButton';
+import s from './components/superButton/SuperButton.module.css'
+import {Adidas} from './components/Adidas';
 
 function App() {
 
-    const [books, setBooks] = useState<BookPropsType[]>([
-        {
-            id: 1,
-            title: 'Klassik',
-            author: 'Griboedov',
-            nameOfBook: 'Gore ot uma',
-            yearOfPublishing: 1825,
-            isChecked: true
-        },
-        {
-            id: 2,
-            title: 'Roman',
-            author: 'Basset',
-            nameOfBook: 'Blablabla',
-            yearOfPublishing: 2003,
-            isChecked: false
-        },
-        {
-            id: 3,
-            title: 'Horror',
-            author: 'King',
-            nameOfBook: 'Door',
-            yearOfPublishing: 1999,
-            isChecked: true
-        },
-        {
-            id: 4,
-            title: 'Klassik',
-            author: 'Tolstoj',
-            nameOfBook: 'Voina i mir',
-            yearOfPublishing: 1869,
-            isChecked: false
-        },
-        {
-            id: 5,
-            title: 'Roman',
-            author: 'Basset',
-            nameOfBook: 'Blablabla2',
-            yearOfPublishing: 2020,
-            isChecked: true
-        },
-    ])
-
-    const [newBookTitle, setNewBookTitle] = useState('')
-
-
-    function addBook() {
-        const newBook = {
-            id: 6,
-            title: '',
-            author: '',
-            nameOfBook: newBookTitle,
-            yearOfPublishing: 0,
-            isChecked: false
-        }
-        setBooks([newBook, ...books])
-        setNewBookTitle('')
-    }
-
+    const tasks = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
 
     return (
-        <div className="App">
-            <h1>{newBookTitle}</h1>
-            <h2>Выберите интересующий Вас продукт:</h2>
-            <div>
-                <Input newBookTitle={newBookTitle} setNewBookTitle={setNewBookTitle}/>
-                <Button nameBtn={'+'} callBack={addBook}/>
-            </div>
-            <Items books={books}/>
+        <div>
+            {/*            <SuperButton
+                onClick={() => {}}
+                color={'blue'}
+                className={s.styleForSuperButtonBlue}
+            >
+
+                SuperButtonBlue
+            </SuperButton>
+            <SuperButton
+                onClick={() => {}}
+                color={'red'}
+                className={s.styleForSuperButtonRed}
+            >
+                SuperButtonRed
+            </SuperButton>*/}
+
+
+            {/*            <SuperButton onClick={() => {}} color={'red'}>SuperButtonRed</SuperButton>
+            <SuperButton onClick={() => {}}>SuperButtonGreen</SuperButton>
+            <SuperButton onClick={() => {}} color={'blue'}>SuperButtonBlue</SuperButton>
+            <SuperButton onClick={() => {}} disabled>SuperButtonBlueDisabled</SuperButton>*/}
+
+            {/*            <Adidas tasks={tasks}>
+                <SuperButton onClick={() => {
+                }} color={'red'}>SuperButtonRed</SuperButton>
+                <SuperButton onClick={() => {
+                }}>SuperButtonGreen</SuperButton>
+                <SuperButton onClick={() => {
+                }} color={'blue'}>SuperButtonBlue</SuperButton>
+                <div>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                </div>
+            </Adidas>
+            <Adidas tasks={tasks}>
+                <SuperButton onClick={() => {
+                }} disabled>SuperButtonBlueDisabled</SuperButton>
+                <div>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                </div>
+            </Adidas>
+            <Adidas tasks={tasks}>
+                gg
+            </Adidas>*/}
+
+{/*            <Modal>
+                <input placeholder="email" type="email"/>
+                <input placeholder="pass" type="password"/>
+                <button>send</button>
+            </Modal>*/}
+
+            <Modal>
+                <h3>Confidient Information</h3>
+                <input placeholder="email" type="email"/>
+                <input placeholder="pass" type="password"/>
+                <input placeholder="pass" type="password"/>
+                <div>
+                    <input type="checkbox"/>
+                    <span>I agree</span>
+                </div>
+                <button>send</button>
+            </Modal>
+
         </div>
     );
 }
 
-export default App;
 
+export default App;
