a090d7896c2e5a6909ba5480222a893c1e84199c
julia-lahtina <j.lakhtin@gmail.com>
2024-03-03 10:09:04 +0100

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,28 @@
 import React, {useState} from 'react';
-import './App.css';
-import {Modal} from './components/modal/Modal';
-import {SuperButton} from './components/superButton/SuperButton';
-import s from './components/superButton/SuperButton.module.css'
-import {Adidas} from './components/Adidas';
+import styles from "./components/Site.module.css";
+import {PageOne} from './components/pages/PageOne';
+import {PageTwo} from './components/pages/PageTwo';
+import {PageThree} from './components/pages/PageThree';
+import {Route, Routes} from 'react-router-dom';
+
 
 function App() {
-
-    const tasks = [
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false}
-    ]
-
     return (
         <div>
-            {/*            <SuperButton
-                onClick={() => {}}
-                color={'blue'}
-                className={s.styleForSuperButtonBlue}
-            >
-
-                SuperButtonBlue
-            </SuperButton>
-            <SuperButton
-                onClick={() => {}}
-                color={'red'}
-                className={s.styleForSuperButtonRed}
-            >
-                SuperButtonRed
-            </SuperButton>*/}
-
-
-            {/*            <SuperButton onClick={() => {}} color={'red'}>SuperButtonRed</SuperButton>
-            <SuperButton onClick={() => {}}>SuperButtonGreen</SuperButton>
-            <SuperButton onClick={() => {}} color={'blue'}>SuperButtonBlue</SuperButton>
-            <SuperButton onClick={() => {}} disabled>SuperButtonBlueDisabled</SuperButton>*/}
-
-            {/*            <Adidas tasks={tasks}>
-                <SuperButton onClick={() => {
-                }} color={'red'}>SuperButtonRed</SuperButton>
-                <SuperButton onClick={() => {
-                }}>SuperButtonGreen</SuperButton>
-                <SuperButton onClick={() => {
-                }} color={'blue'}>SuperButtonBlue</SuperButton>
-                <div>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    Здесь будет навигация
                 </div>
-            </Adidas>
-            <Adidas tasks={tasks}>
-                <SuperButton onClick={() => {
-                }} disabled>SuperButtonBlueDisabled</SuperButton>
-                <div>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
-                    <span>Lorem ipsum dolor sit amet, consectetur.</span>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path={'/page1'} element={<PageOne/>}/>
+                        <Route path={'/page2'} element={<PageTwo/>}/>
+                        <Route path={'/page3'} element={<PageThree/>}/>
+                    </Routes>
                 </div>
-            </Adidas>
-            <Adidas tasks={tasks}>
-                gg
-            </Adidas>*/}
-
-{/*            <Modal>
-                <input placeholder="email" type="email"/>
-                <input placeholder="pass" type="password"/>
-                <button>send</button>
-            </Modal>*/}
-
-            <Modal>
-                <h3>Confidient Information</h3>
-                <input placeholder="email" type="email"/>
-                <input placeholder="pass" type="password"/>
-                <input placeholder="pass" type="password"/>
-                <div>
-                    <input type="checkbox"/>
-                    <span>I agree</span>
-                </div>
-                <button>send</button>
-            </Modal>
-
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
     );
 }
