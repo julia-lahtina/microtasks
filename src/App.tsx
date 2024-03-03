63f362f5a281ca56c13c50a91fa48cb5dc7e804e
julia-lahtina <j.lakhtin@gmail.com>
2024-03-03 11:29:40 +0100

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from "./components/Site.module.css";
 import {PageOne} from './components/pages/PageOne';
 import {PageTwo} from './components/pages/PageTwo';
 import {PageThree} from './components/pages/PageThree';
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {Error404} from './components/pages/Error404';
 
 
 function App() {
@@ -16,9 +17,14 @@ function App() {
                 </div>
                 <div className={styles.content}>
                     <Routes>
+                        <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
+
                         <Route path={'/page1'} element={<PageOne/>}/>
                         <Route path={'/page2'} element={<PageTwo/>}/>
                         <Route path={'/page3'} element={<PageThree/>}/>
+
+                        <Route path={'/error404'} element={<Error404/>}/>
+                        <Route path={'/*'} element={<Navigate to={'/error404'}/>}/>
                     </Routes>
                 </div>
             </div>
